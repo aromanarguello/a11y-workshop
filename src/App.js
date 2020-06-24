584a35dca5cfd0eceea31983c6beea5d5b8edf74
Alejandro <aromanarguello@gmail.com>
2020-06-24 16:59:30 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to our shop!</h1>
+      <h1 className="subtitle">This is a super awesome store!</h1>
+      {/* eslint-disable-next-line */}
+      <img
+        style={{ width: "50px", height: "50px" }}
+        src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+      />
+      <div class="header">
+        <div>Home</div>
+        <div>Shop</div>
+        <div>Cart</div>
+      </div>
+      <p>Item Name:</p>
+      <input />
+      <button>Search!</button>
     </div>
   );
 }
